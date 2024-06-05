c637f5b4c984c5557699d993f6e99496081c1039
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-06-06 00:17:20 +0100

    feat: Replace hono validator with typia-validator
    This commit replaces the hono validator with the typia-validator in the
    vite-hono example. The typia-validator is imported from the
    @hono/typia-validator package. The validation function is also updated
    from 'check' to 'validate'. The package.json file is updated to include
    the @hono/typia-validator package in the devDependencies.

--- examples/vite-hono/main.ts
+++ examples/vite-hono/main.ts
@@ -1,37 +1,31 @@
 import { Hono } from "hono";
-import { validator } from 'hono/validator'
+import { typiaValidator } from '@hono/typia-validator'
 import typia, { type tags } from "typia";
 
 /** define the author interface */
 interface Author {
   name: string;
   age:
-    & number
-    & tags.Type<"uint32">
-    & tags.Minimum<20>
-    & tags.ExclusiveMaximum<100>;
+  & number
+  & tags.Type<"uint32">
+  & tags.Minimum<20>
+  & tags.ExclusiveMaximum<100>;
 }
 
 /** create a validate function */
-const check = typia.createIs<Author>();
+const validate = typia.createValidate<Author>();
 
 
 const app = new Hono();
 
 app.post("/",
-  validator('json', (v, c)=>{
-    const result = check(v);
-    if (!result){
-      return c.json({success: false, message: 'Invalid data', data: v})
-    }
-    return v
-  })
-  , (c) => {
-  const data = c.req.valid("json");
-  return c.json({
-    success: true,
-    message: `${data.name} is ${data.age}`,
+  typiaValidator('json', validate),
+  (c) => {
+    const data = c.req.valid("json");
+    return c.json({
+      success: true,
+      message: `${data.name} is ${data.age}`,
+    });
   });
-});
 
 export default app;
