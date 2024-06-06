22b4900ee1dd8e462604c09159810f7dbd342075
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-06-07 00:55:52 +0100

    feat(unplugin-typia): add support for bun
    A new entry file for bun plugin has been added. The plugin can now be
    used with bun. The changes include:
    
    - Addition of a new file `src/bun.ts` which contains the implementation
      for the bun plugin.
    - The `package.json` file has been updated to include the new entry
      point for the bun plugin.
    - The `jsr.json` file has also been updated to reflect the new entry
      point.
    - The `bun.lockb` file has been updated with the new dependencies.
    
    BREAKING CHANGE: The plugin now requires bun to be present in the
    global scope.

---
+++ packages/unplugin-typia/src/bun.ts
@@ -0,0 +1,73 @@
+/**
+ * This entry file is for bun plugin.
+ *
+ * @module
+ */
+
+import type { BunPlugin } from 'bun';
+import { type Options, resolveOptions, transformTypia } from './api.js';
+import { defaultOptions } from './core/options.js';
+
+if (globalThis.Bun == null) {
+	throw new Error('You must use this plugin with bun');
+}
+
+/**
+ * bun plugin
+ *
+ * @example
+ * ```ts
+ * // preload.ts
+ * import { plugin } from 'bun';
+ * import UnpluginTypia from 'unplugin-typia/bun'
+ *
+ * plugin(UnpluginTypia({ /* your options *\/}))
+ * ```
+ * ```toml
+ * // bunfig.toml
+ * preload = ["./preload.ts"]
+ *
+ * [test]
+ * preload = ["./preload.ts"]
+ * ```
+ */
+function bunTypiaPlugin(
+	options?: Options,
+): BunPlugin {
+	const bunPlugin = ({
+		name: 'unplugin-typia',
+		setup(build) {
+			const resolvedOptions = resolveOptions(options ?? {});
+			const { include } = resolvedOptions;
+			const filter = include instanceof RegExp
+				? include
+				: typeof include === 'string'
+					? new RegExp(include)
+					: Array.isArray(include) && include[0] instanceof RegExp
+						? include[0]
+						: defaultOptions.include[0];
+
+			build.onLoad({ filter }, async (args) => {
+				const { path } = args;
+
+				const source = await Bun.file(path).text();
+
+				const res = await transformTypia(
+					path,
+					source,
+					{ warn: console.warn } as Parameters<typeof transformTypia>[2],
+					resolvedOptions,
+				);
+
+				return {
+
+					contents: res?.code ?? source,
+				};
+			});
+		},
+	}) as const satisfies BunPlugin;
+
+	return bunPlugin;
+}
+
+export default bunTypiaPlugin;
