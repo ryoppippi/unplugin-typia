9cd65fbabda67a6702bba603f6823a169a77430f
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-06-07 01:38:05 +0100

    feat(example/bun-build): Add JSON schema generation for IMember
    This commit introduces a new feature where a JSON schema is generated
    for the IMember interface. This schema includes the email, id, and age
    fields with their respective constraints and descriptions. The schema
    is logged to the console in a pretty-printed format for easy viewing.

--- examples/bun-build/index.ts
+++ examples/bun-build/index.ts
@@ -7,8 +7,20 @@ import { randomUUID } from 'crypto';
 
 /** define the schema of the member */
 interface IMember {
+  /**
+  * email of the member
+  */
   email: string & tags.Format<"email">;
+
+  /**
+  * id of the member
+  */
   id: string & tags.Format<"uuid">;
+  
+  /**
+  * age of the member
+  * age should be greater than 19
+  */
   age: number & tags.ExclusiveMinimum<19> & tags.Maximum<100>;
 }
 
@@ -44,4 +56,56 @@ console.log({ member, is: is(member) }) // true
 /** create protobuf schema */
 const MemberSchema = typia.protobuf.message<IMember>()
 console.log(MemberSchema)
+
+/**
+* example: create json schema in builid time
+*
+* @output
+* ```
+* {
+*   "version": "3.0",
+*   "components": {
+*     "schemas": {
+*       "IMember": {
+*         "type": "object",
+*         "properties": {
+*           "email": {
+*             "type": "string",
+*             "format": "email",
+*             "description": "email of the member"
+*           },
+*           "id": {
+*             "type": "string",
+*             "format": "uuid",
+*             "description": "id of the member"
+*           },
+*           "age": {
+*             "type": "number",
+*             "exclusiveMinimum": true,
+*             "minimum": 19,
+*             "maximum": 100,
+*             "description": "age of the member\nage should be greater than 19"
+*           }
+*         },
+*         "nullable": false,
+*         "required": [
+*           "email",
+*           "id",
+*           "age"
+*         ],
+*         "description": "define the schema of the member"
+*       }
+*     }
+*   },
+*   "schemas": [
+*     {
+*       "$ref": "#/components/schemas/IMember"
+*     }
+*   ]
+* }
+* ```
+*/
+
+const MemberJsonSchema = typia.json.application<[IMember], '3.0'>()
+console.log(JSON.stringify(MemberJsonSchema, null, 2))
 
