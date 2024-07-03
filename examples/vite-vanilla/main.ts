feacabd36cdd5f27957910635fa9c1cf763e738a
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-03 18:17:34 +0100

    feat(examples/vite-vanilla): update example

--- examples/vite-vanilla/main.ts
+++ examples/vite-vanilla/main.ts
@@ -1,6 +1,5 @@
 import typia, { type tags } from "typia";
 
-export const random = typia.createRandom<IMember>();
 
 interface IMember {
   email: string & tags.Format<"email">;
@@ -8,5 +7,13 @@ interface IMember {
   age: number & tags.ExclusiveMinimum<19> & tags.Maximum<100>;
 }
 
-const dummy = random();
-document.getElementById("app")!.innerHTML = JSON.stringify(dummy);
+const dummyData = {
+  email: "example@example.com",
+  id: `2f3cb112-102d-45ae-a1ba-88f4c46d5bf5`,
+  age: 20
+} as const satisfies IMember;
+
+const res = typia.validate<IMember>(dummyData);
+
+document.getElementById("app")!.innerHTML = res.success ? "Success" : JSON.stringify(res.errors, null, 2);
+
