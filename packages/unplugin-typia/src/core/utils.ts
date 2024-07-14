dd46217efdfd762038f85de6e5b98e13421bd0ec
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-14 23:43:32 +0100

    feat: add dynamicImport utility function (fixes: fixes: #198)
    This commit introduces a new utility function, dynamicImport, which
    is used to dynamically import modules. This function is now used in
    the Svelte language file to import the Svelte compiler. This change
    improves the code readability and maintainability.

--- packages/unplugin-typia/src/core/utils.ts
+++ packages/unplugin-typia/src/core/utils.ts
@@ -10,3 +10,11 @@ export function log(
 export function isBun() {
 	return globalThis.Bun != null;
 }
+
+/**
+ * Dynamic import a module.
+ * Because JSR fails to resolve dynamic import, we have to use this workaround. (see: https://github.com/ryoppippi/unplugin-typia/issues/198)
+ */
+export async function dynamicImport<T extends string>(specifier: T): Promise<unknown> {
+	return import(specifier);
+}
