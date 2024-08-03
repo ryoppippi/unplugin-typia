5e0a9f7b285d634aff4c1f5685499f2e020a4189
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-03 22:50:02 +0100

    fix(test): use resolve instead of join in _get_fixtures.ts
    This commit replaces the 'join' function from 'pathe' with 'resolve' in
    _get_fixtures.ts. This change ensures the correct resolution of paths
    across different environments.

--- packages/unplugin-typia/test/_get_fixtures.ts
+++ packages/unplugin-typia/test/_get_fixtures.ts
@@ -1,12 +1,12 @@
 import { $ } from 'bun';
-import { join } from 'pathe';
+import { resolve } from 'pathe';
 import type { ID, Source } from '../src/core/types.js';
 
-const ids = await $`ls ${join(import.meta.dirname, './fixtures')}/*.ts`.text().then(x => x.split('\n').filter(Boolean)) as ID[ ];
+const ids = await $`ls ${resolve(__dirname, './fixtures')}/*.ts`.text().then(x => x.split('\n').filter(Boolean)) as ID[ ];
 
 const fixtures: [ID, Source][] = [];
 for (const id of ids) {
 	fixtures.push([id, await $`cat ${id}`.text() as Source]);
 }
 
-export { fixtures, ids };
+export { fixtures };
