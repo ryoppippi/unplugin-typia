9f5ea808320530ddce0a281438e172b72be4261f
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-29 22:38:03 +0100

    feat(examples/sveltekit): add sakura.css and update SvelteKit example
    - Added sakura.css dependency to the project.
    - Updated bun.lockb with sakura.css details.
    - Modified SvelteKit example to use sakura.css for styling.
    - Replaced uuid usage with crypto.randomUUID().
    - Updated validation logic and error display in SvelteKit example.

---
+++ examples/sveltekit/src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+  import 'sakura.css/css/normalize.css';
+  import 'sakura.css/css/sakura-dark.css';
+
+  const { children } = $props();
+</script>
+
+{@render children()}
--- examples/sveltekit/src/routes/+page.svelte
+++ examples/sveltekit/src/routes/+page.svelte
@@ -1,33 +1,30 @@
 <script lang="ts">
-  import { v4 } from "uuid";
-  import typia, { type tags } from "typia";
+import typia, { type tags } from "typia";
 
-  interface IMember {
-    id: string & tags.Format<"uuid">;
-    email: string & tags.Format<"email">;
-    age: number & tags.ExclusiveMinimum<19> & tags.Maximum<100>;
-  }
+interface IMember {
+  id: string & tags.Format<"uuid">;
+  email: string & tags.Format<"email">;
+  age: number & tags.ExclusiveMinimum<19> & tags.Maximum<100>;
+}
 
-  const check = typia.createIs<IMember>();
+const validate = typia.createValidate<IMember>();
 
-  let member = $state<IMember>({
-    id: v4(),
-    email: "samchon.github@gmai19l.com",
-    age: 20,
-  })
+let member = $state<IMember>({
+  id: crypto.randomUUID(),
+  email: "samchon.github@gmai19l.com",
+  age: 20,
+})
 
-  let isValid = $derived(check(member))
+let validation = $derived(validate(member))
 </script>
 
-<h1>
-  {isValid ? "Valid" : "Invalid"}
-</h1>
+<h1> {validation.success ? "Valid" : "Invalid"} </h1>
 
-<div>
-  <p>{member.id}</p>
-</div>
-
-<div style="display: flex; flex-direction: column;">
+<div style="display: flex; flex-direction: column; width: 100%;">
+  <div>
+    <label for="id">ID</label>
+    <input type="text" id="id" value={member.id} disabled />
+  </div>
   <div>
     <label for="email">Email</label>
     <input type="text" id="email" bind:value={member.email} />
@@ -38,3 +35,11 @@
   </div>
 </div>
 
+{#if !validation.success}
+  <p> errors: </p>
+  <ul style="color: red;">
+    {#each validation.errors as error, i}
+      <li>{JSON.stringify(error)}</li>
+    {/each}
+  </ul>
+{/if}
