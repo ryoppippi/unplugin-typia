537a3555e2b1be25c250dc1c3e2fc064f1d0f17e
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-09-12 15:38:16 +0100

    feat(examples/vite-react): add  vite-plugin-inspect to analyse sourcemap

--- examples/vite-react/vite.config.ts
+++ examples/vite-react/vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import UnpluginTypia from "@ryoppippi/unplugin-typia/vite";
+import Inspect from 'vite-plugin-inspect'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    Inspect(),
     UnpluginTypia({}),
     react(),
   ],
