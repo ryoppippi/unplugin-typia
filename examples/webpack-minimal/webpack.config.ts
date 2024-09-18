c68d2e1608239d7d8976429b681f365ccbfbf6ac
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-09-19 00:02:57 +0100

    feat(examples/webpack): simplify webpack config

--- examples/webpack-minimal/webpack.config.ts
+++ examples/webpack-minimal/webpack.config.ts
@@ -11,55 +11,45 @@ const {default: UnpluginTypia}  = tsx.require('../../packages/unplugin-typia/src
 const isProduction = process.env.NODE_ENV == 'production';
 
 
-const config: Configuration = {
-    entry: './src/index.ts',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devServer: {
-        open: true,
-        host: 'localhost',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html',
-        }),
-        UnpluginTypia()     
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)$/i,
-                loader: 'ts-loader',
-                exclude: ['/node_modules/'],
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
-            },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.jsx', '.js', '...'],
-    },
-};
-
 module.exports = () => {
-    if (isProduction) {
-        config.mode = 'production';
-        
-        if (config.plugins == null) {
-            config.plugins = []
-        }
-        config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
-        
-    } else {
-        config.mode = 'development';
-    }
-    return config;
+    return {
+        mode: isProduction ? 'production' : 'development',
+        entry: './src/index.ts',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+        },
+        devServer: {
+            open: true,
+            host: 'localhost',
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: 'index.html',
+            }),
+            UnpluginTypia(),
+            isProduction && new WorkboxWebpackPlugin.GenerateSW(),
+            // Add your plugins here
+            // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.(ts|tsx)$/i,
+                    loader: 'ts-loader',
+                    exclude: ['/node_modules/'],
+                },
+                {
+                    test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                    type: 'asset',
+                },
+
+                // Add your rules for custom modules here
+                // Learn more about loaders from https://webpack.js.org/loaders/
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.jsx', '.js', '...'],
+        },
+    } as const satisfies Configuration;
 };
+
