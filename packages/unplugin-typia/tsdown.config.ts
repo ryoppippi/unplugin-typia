86e824d1a340f9b968cf0a4e171efac7865eac3b
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-06-10 19:38:51 +0100

    feat: build bun plugin

--- packages/unplugin-typia/tsdown.config.ts
+++ packages/unplugin-typia/tsdown.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'tsdown';
 const config: ReturnType<typeof defineConfig> = defineConfig({
 	entry: [
 		'src/*.ts',
-		'!src/bun.ts',
 	],
 	define: {
 		'import.meta.vitest': 'undefined',
@@ -16,7 +15,6 @@ const config: ReturnType<typeof defineConfig> = defineConfig({
 	sourcemap: true,
 	unused: {
 		level: 'error',
-		ignore: ['bun-only'],
 	},
 	publint: true,
 });
